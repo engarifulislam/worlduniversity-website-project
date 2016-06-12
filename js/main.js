18d99dca4de6407fa00e02fcf5ed48dec6b2aa61
engarifulislam <eng.arifulislam2@gmail.com>
2016-06-12 20:46:23 +0600

    change slider jquery

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@
 
         $('[data-toggle="tooltip"]').tooltip();
 
+		$('.my-slider').unslider();
+
 
 
 
@@ -24,3 +26,15 @@
 
 
 }(jQuery));
+
+//Slider Code
+//
+//jQuery(function($) {
+//	$('.slider').sss();
+//
+//		slideShow : true, // Set to false to prevent SSS from automatically animating.
+//		startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
+//		transition : 400, // Length (in milliseconds) of the fade transition.
+//		speed : 3500, // Slideshow speed in milliseconds.
+//		showNav : true // Set to false to hide navigation arrows.
+//});
